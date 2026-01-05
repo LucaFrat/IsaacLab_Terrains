10f7a3b44153e8625451d900052469e23d4a95fd
LucaFrat <luca_frattini@outlook.com>
2026-01-05 23:18:46 +0100

    add play environment for pos task

--- source/IsaacLab_Terrains/IsaacLab_Terrains/tasks/manager_based/locomotion/position/config/anymal_c/__init__.py
+++ source/IsaacLab_Terrains/IsaacLab_Terrains/tasks/manager_based/locomotion/position/config/anymal_c/__init__.py
@@ -26,17 +26,18 @@ gym.register(
     },
 )
 
-# gym.register(
-#     id="Isaac-Position-Rough-Anymal-C-Play-v0",
-#     entry_point="isaaclab.envs:ManagerBasedRLEnv",
-#     disable_env_checker=True,
-#     kwargs={
-#         "env_cfg_entry_point": f"{__name__}.rough_env_cfg:AnymalCRoughEnvCfg_PLAY",
-#         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCRoughPPORunnerCfg",
-#         "rsl_rl_with_symmetry_cfg_entry_point": (
-#             f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCRoughPPORunnerWithSymmetryCfg"
-#         ),
-#         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_rough_ppo_cfg.yaml",
-#         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
-#     },
-# )
+
+gym.register(
+    id="Isaac-Position-Rough-Anymal-C-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:AnymalCRoughEnvCfg_Play",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCRoughPPORunnerWithSymmetryCfg",
+        "rsl_rl_with_symmetry_cfg_entry_point": (
+            f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalCRoughPPORunnerWithSymmetryCfg"
+        ),
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_rough_ppo_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
+    },
+)
